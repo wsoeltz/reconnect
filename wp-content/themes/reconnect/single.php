c3a5bb4fd04a4bd5813c959e0b643d54e240ab99
Kyle Soeltz <skiis652@gmail.com>
2018-02-07 17:16:14 -0500

    Added similar posts to end of a post

--- wp-content/themes/reconnect/single.php
+++ wp-content/themes/reconnect/single.php
@@ -39,6 +39,51 @@ get_header(); ?>
 					'<span class="post-title">%title</span>',
 			) );*/
 
+			// Get categories
+	        $categories = wp_get_post_terms( get_the_ID(), 'category');
+
+	        // Check if there are any categories
+	        if( ! empty( $categories ) ) :
+
+	            // Get all posts within current category, but exclude current post
+	            $category_posts = new WP_Query( array(
+	                'cat'          => $categories[0]->term_id,
+	                'post__not_in' => array( get_the_ID() ),
+	            ) );
+
+	            // Check if there are any posts
+	            $i = 0;
+	            if( $category_posts->have_posts() ) :
+	                // Loop trough them
+	            	echo '<div class="inner">';
+	            	echo '<h2>Similar Posts</h2>';
+	                while( $category_posts->have_posts() && $i < 4 ) : $category_posts->the_post();
+	                    // Display posts
+	                    // echo get_the_title() . '<br />';
+	                    echo '<div class="card thumbnail-left matchHeight group">';
+						echo '<a class="thumbnail-image" style="background-image:url(';
+						the_post_thumbnail_url('full');
+						echo ')" href="';
+						echo get_permalink();
+						echo '"></a>';
+						the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+						echo '<span class="entry-details">';
+						echo get_the_date('m.d.Y');
+						echo ' - ';
+						echo get_the_author();
+						echo '</span>';
+						echo '<p>';
+						echo excerpt_inline(30);
+						// echo excerpt(30);
+						echo '</p>';
+						echo '</div>';
+	                    $i++;
+	                endwhile;
+	                echo '</div>';
+	            endif;
+	        endif;
+	        wp_reset_postdata();
+
 		// End the loop.
 		endwhile;
 		?>
